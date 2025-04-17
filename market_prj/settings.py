2704c5782c86314bf7c896845c0d4de998ccf37d
abyssian03 <abyssian03@gmail.com>
2025-04-17 18:02:46 +0300

    Working version

--- market_prj/settings.py
+++ market_prj/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mainapp',
+    'authapp',
 ]
 
 MIDDLEWARE = [
@@ -91,6 +92,8 @@ DATABASES = {
 
 }
 
+
+AUTH_USER_MODEL = 'authapp.TravelUser'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
