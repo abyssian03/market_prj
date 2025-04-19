be653b4d0b239a78ea2ae4930f0de575459ba1f0
abyssian03 <abyssian03@gmail.com>
2025-04-19 15:01:25 +0300

    Working version

--- market_prj/urls.py
+++ market_prj/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('list_of_accommodations/', include('mainapp.urls', namespace='acc')),
     path('auth/', include('authapp.urls', namespace='auth')),
     path('', include('social_django.urls', namespace='social')),
+    path('admin/', include('adminapp.urls', namespace='admin')),
 ]
 
 if settings.DEBUG:
